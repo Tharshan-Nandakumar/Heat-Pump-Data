4a6d00131917305fffcb119d57ed4b23a7dadbf6
Tharshan <tharshan1465@gmail.com>
2024-10-22 18:04:36 +0100

    app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -64,6 +64,11 @@ import {
 } from "@azure/msal-react";
 import { loginRequest } from "./Components/Auth/auth-config";
 import Inputs from "./Components/Inputs/Inputs";
+import Map from "./Components/Map/map";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Rendesco from "./Components/Images/Rendesco.png";
+import Navbar from "./Components/Navbar/Navbar";
+import Wizard from "./Components/Wizard/Wizard";
 
 function App() {
   const { instance } = useMsal();
@@ -85,32 +90,86 @@ function App() {
     ///window.location.reload();
   };
 
+  const styles = {
+    fontFamily: "'Segoe UI', monospace",
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={styles}>
       <AuthenticatedTemplate>
         {activeAccount ? (
           <>
-            <div style={{ padding: "10px" }}>
-              <button
-                onClick={handleLogoutRedirect}
-                style={{ margin: " 10px 0 0 85%", width: "10%" }}
-              >
-                Logout
-              </button>
-              <Inputs />
+            <div style={{ padding: "0" }}>
+              <Navbar handleLogoutRedirect={handleLogoutRedirect} />
+              <Router>
+                <Routes>
+                  <Route path="/" element={<Map />} />
+                  <Route path="live_data/:site" element={<Inputs />} />
+                  <Route path="diagnostic_wizard" element={<Wizard />} />
+                </Routes>
+              </Router>
             </div>
           </>
         ) : null}
       </AuthenticatedTemplate>
       <UnauthenticatedTemplate>
-        <h1 className="title" style={{ margin: " 10% 5% 20px 5%" }}>
-          Heat Pump Data
+        <div style={{ overflow: "hidden" }}>
+          <svg
+            height="70vh"
+            viewBox="0 0 1070 1905"
+            fill="none"
+            overflow="hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            style={{
+              scale: "8",
+              position: "fixed", // Place it in the background
+              top: "20%", // Adjust as needed
+              left: "120%", // Adjust as needed
+              opacity: 0.2, // Make it faint
+              zIndex: -1, // Ensure it's in the background
+              pointerEvents: "none",
+              overflow: "hidden",
+            }}
+          >
+            <path
+              d="M650 1503V0L0 799.5L650 1503Z"
+              fill="#299F76"
+              style={{ overflow: "hidden" }}
+            />
+            <path
+              d="M650 310L19 1106.56L650 1801"
+              stroke="#CCCCCC"
+              strokeWidth="9"
+              style={{ overflow: "hidden" }}
+            />
+          </svg>
+        </div>
+        <img id="logo_main" src={Rendesco} alt="Rendesco logo" />
+        <h1 className="title" style={{ margin: " 23% 5% 20px 5%" }}>
+          Operations and Maintenance
         </h1>
         <button
           onClick={handleLoginRedirect}
-          style={{ margin: " 10px 5% 5% 5%", width: "90%" }}
+          style={{ margin: "3% auto", width: "120px" }}
         >
+          <img
+            src="https://cdn.prod.website-files.com/660e750b3c8e9f9fcab9f476/660e7bbcc761116366a6fe1e_Rendesco%20logo%20symbol.webp"
+            loading="lazy"
+            width="20%"
+            height="20%"
+            alt="A green and white logo Rendesco Heat Support"
+          />
           Login
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1rem"
+            viewBox="0 0 448 512"
+          >
+            <path
+              d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
+              fill="#299f76"
+            ></path>
+          </svg>
         </button>
       </UnauthenticatedTemplate>
     </div>
